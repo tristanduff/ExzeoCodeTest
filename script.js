67b85ed26c3d459bb74e2c8e3a45d954b2103356
tristanduff <tristan.duff@gmail.com>
2016-08-10 17:42:20 -0500

    Scripting used to populate data in default.html

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 $.getJSON("https://itunes.apple.com/us/rss/topalbums/limit=100/json", { get_param: "value" }, function(data) { //bracket lvl 1, jsonget		//Grabs data from iTunes
 	
-	console.log(data.feed.entry); //Runs through the JSON feed in console
+	console.log(data.feed.entry); 																											//Runs through the JSON feed in console
 	
-	var topDiv = $('<div class="flexContainerTop"></div>');
-	//use topDiv.appendTo
-	
+	var topDiv = $('<div class="flexContainerAlbumList"></div>');
+	//Can "Top 100 iTunes Songs" text be placed up here?
+    var ranking = 1;
+    	
     	($.each(data.feed.entry, function(index, element) {	//bracket lvl 2, datafeed														//Array for populating the iTunes data
-    		
-    		var divContainer = $('<div align="center">').appendTo(topDiv); //should put the divContainer in the top div
+    		//var divRank = $('<span>').prependTo(topDiv);
+    		var divContainer = $('<div align="center">').appendTo(topDiv); 																	//puts the divContainer at end of topDiv
     		var linebreak1 = $('<br></br>');
     		var linebreak2 = $('<br></br>');
-    		var linebreak3 = $('<br></br>');    	
+    		var linebreak3 = $('<br></br>'); 
+    		var linebreak4 = $('<br></br>');   	
+    		
+    		//var imgtag = $('<img src=');	
     			
-			//var albumart = $('<span>', { //bracket lvl 3, albumart																			//Generates the "AlbumArt" element
-			//text: element["im:image"][2].label 																								//fetches album art .jpg link
-			//}); //bracket lvl 3, albumart
-
-    		var name = $('<span>', { //bracket lvl 3, name																					//Generates the "Name" element
+			var albumart = $('<span>', { //bracket lvl 3, albumart																			
+			text: element["im:image"][2].label 																								//fetches album art .jpg link
+			}); //bracket lvl 3, albumart
+	
+			var img = $("<img />").attr('src', element["im:image"][0].label);
+	
+    		var name = $('<span>', { //bracket lvl 3, name																					
     	    text: element["im:name"].label																									//fetches name label
     		}); //bracket lvl 3, name					
     		
-    		var artist = $('<span>', {	//bracket lvl 3, artist																				//Generates the "Artist" element
+    		var artist = $('<span>', {	//bracket lvl 3, artist																				
     	    text: element["im:artist"].label																								//fetches artist label
     		}); //bracket lvl 3, artist
     	
-    		var releasedate = $('<span>', {
+    		var releasedate = $('<span>', { //bracket lvl 3, releasedate
     		text:element["im:releaseDate"].attributes.label
-    		});
+    		}); //bracket lvl 3, releasedate
     	
-    	divContainer.addClass("flexItemTop")
+    	//divRank.addClass("flexItemRank")
+    	divContainer.addClass("flexItemAlbumEntry")
         linebreak1.addClass("LineBreak")
         linebreak2.addClass("LineBreak")
         linebreak3.addClass("LineBreak")
-        //albumart.addClass("AlbumArt")																										
+        linebreak4.addClass("LineBreak")
+        albumart.addClass("AlbumArt")																									
+        name.addClass("Name")        
         artist.addClass("Artist")																											
-        name.addClass("Name")
-        releasedate.addClass("ReleaseDate")		
-        																							
-       	divContainer.append(name, linebreak2, artist, linebreak3, releasedate);  																	
-       	divContainer.appendTo(topDiv);                                    																
+        releasedate.addClass("ReleaseDate")
+        //divRank.append(ranking);
+        //divRank.prependTo(topDiv);																						
+       	divContainer.append(ranking, linebreak1, img, linebreak2, name, linebreak3, artist, linebreak4, releasedate);  																	
+       	divContainer.appendTo(topDiv);    
+       	ranking = ranking+1;                                																
        	})); //bracket lvl 2, datafeed
        	
        	
-       	$('body').append(topDiv); //This'll be what puts the data into the page, hopefully
+       	$('body').append(topDiv);
        	
 }); //bracket lvl 1, jsonget
